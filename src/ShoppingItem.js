141a88ed8b02f7498ea231ef588d010a614387c4
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2018-11-20 11:30:13 -0200

    Documentação

--- src/ShoppingItem.js
+++ src/ShoppingItem.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+/**
+ * Componente funcional stateless responsável pela geração do item da lista de compra
+ * @param {object} props objeto contendo as propriedades repassadas pelo componente pai
+ */
 function ShoppingItem(props){
     return (<li>{props.item}</li>)
 }
 
+/**
+ * Validação dos tipos obrigatórios que devem ser repassados pelo componente pai
+ */
 ShoppingItem.propTypes = {
     item: PropTypes.string.isRequired
 }
 
+/**
+ * Exportação do componente para uso
+ */
 export default ShoppingItem
