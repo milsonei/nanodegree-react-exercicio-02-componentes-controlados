6f1fe1026a575e120c8dd3f461a28754c14ad641
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2018-11-20 11:31:54 -0200

    Documentação, inclusão da validação das props e inclusão de um manipulador para o evento onDelete passado pelo componente pai

--- src/DeleteLastItemButton.js
+++ src/DeleteLastItemButton.js
@@ -1,13 +1,29 @@
 import React from 'react'
-
+import PropTypes from 'prop-types'
+/**
+ * Componente funcional responsável por excluir um item da lista de compras
+ * @param {object} props 
+ */
 function DeleteLastItemButton(props){
-  
-    const {deleteLastItemCallback, disableButtonCallback} = props;
+   
+   const handleDeleteLastItem = event => props.onDeleteLastItem();
+
     return (
-        <button onClick={deleteLastItemCallback} disabled={disableButtonCallback()}>
+        <button onClick={handleDeleteLastItem} disabled={props.buttonDisabled}>
         Delete Last Item
       </button>
     )
 }
 
+/**
+ * Validação dos tipos obrigatórios que devem ser repassados pelo componente pai
+ */
+DeleteLastItemButton.propTypes = {
+    buttonDisabled: PropTypes.bool.isRequired,
+    onDeleteLastItem: PropTypes.func.isRequired,
+};
+
+/**
+ * Exportação do componente para uso
+ */
 export default DeleteLastItemButton
