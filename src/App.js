4d41ae58f9dbdfb4f5f77089886329fb99ae92f6
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2018-11-20 08:30:01 -0200

    Modularização do aplicativo em componentes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ShoppingListForm from './ShoppingListForm'
+import DeleteLastItemButton from './DeleteLastItemButton'
+import ShoppingList from './ShoppingList'
 
 class App extends React.Component {
   state = {
@@ -39,24 +42,12 @@ class App extends React.Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Shopping List</h2>
-        <form onSubmit={this.addItem}>
-          <input
-            type="text"
-            placeholder="Enter New Item"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-          <button disabled={this.inputIsEmpty()}>Add</button>
-        </form>
-
-        <button onClick={this.deleteLastItem} disabled={this.noItemsFound()}>
-          Delete Last Item
-        </button>
-
-        <p className="items">Items</p>
-        <ol className="item-list">
-          {this.state.items.map((item, index) => <li key={index}>{item}</li>)}
-        </ol>
+        <ShoppingListForm  inputValue={this.state.value} 
+                           handleInputChange={this.handleChange} 
+                           addItemOnSubmitCallback={this.addItem}
+                           disableButtonCallback={this.inputIsEmpty}/>
+        <DeleteLastItemButton deleteLastItemCallback={this.deleteLastItem} disableButtonCallback={this.noItemsFound}/>
+        <ShoppingList items={this.state.items}/>              
       </div>
     );
   }
